71ae147ab83113b0018d01da9a404d34d03ee6a6
d0lphy1111 <d0lphyy@mail.ru>
2025-01-13 22:36:17 +0300

    коменнтарий свой любой

--- ege25.py
+++ ege25.py
@@ -3,3 +3,5 @@ from fnmatch import fnmatch
 for x in range(1917, 10 ** 10 + 1, 1917):
     if fnmatch(str(x),'3?12?14*5'):
         print(x, x //  1917)
+
+print('lox')
--- testege.py
+++ testege.py
@@ -5,5 +5,6 @@ def f(N):
     else:
         b = '1' + b + '01'
     return int(b,2)
-print(max([f(N) for N in range(0,10 ** 5) if N <= 12]))
+print(max([f(N) for N in range(0,200) if N <= 12]))
 
+print('lox')
